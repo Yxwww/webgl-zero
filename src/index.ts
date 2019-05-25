15d6826e33992fbe0fc8a98cedf9aedb5d0189f1
Yxwww <yuxi.wangcd@gmail.com>
2019-05-25 11:17:34 -0600

    Add index.html setup hmr

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-const haha = 'hello';
+import { join } from "ramda";
 
-// if (module.hot) {
-//     module.hot.accept('./print.js', function() {
-//         console.log('Accepting the updated printMe module!');
-//         printMe();
-//     })
-// }
-export default haha;
+function component() {
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
+
+    element.innerHTML = join(' ')(['Hello', 'webpack']);
+
+    btn.innerHTML = 'Click me and check the console!';
+    // btn.onclick = printMe;  // onclick event is bind to the original printMe function
+
+    element.appendChild(btn);
+
+    return element;
+}
+
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element);
+
+if (module.hot) {
+    module.hot.accept('./app.tsx', function() {
+        console.log('Accepting the updated printMe module!');
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -22,7 +22,7 @@ module.exports = {
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Hot Module Replacement"
+      template: path.resolve(__dirname, "index.html")
     }),
     new webpack.HotModuleReplacementPlugin()
   ],

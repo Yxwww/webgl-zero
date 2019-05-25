27dcf291899b78681329768f3c8a8067ef5e2caf
Yxwww <yuxi.wangcd@gmail.com>
2019-05-25 11:23:38 -0600

    Add canvas on page

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
-import { join } from "ramda";
+function component(): HTMLCanvasElement {
+    const canvas = document.createElement('canvas');
+    canvas.width = 600;
+    canvas.height = 600;
+    canvas.style.border = '1px solid grey';
 
-function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-
-    element.innerHTML = join(' ')(['Hello', 'webpack']);
-
-    btn.innerHTML = 'Click me and check the console!';
-    // btn.onclick = printMe;  // onclick event is bind to the original printMe function
-
-    element.appendChild(btn);
-
-    return element;
+    return canvas;
 }
 
-let element = component(); // Store the element to re-render on print.js changes
-document.body.appendChild(element);
+let canvas = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(canvas);
 
 if (module.hot) {
     module.hot.accept('./app.tsx', function() {

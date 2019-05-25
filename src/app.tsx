a9be396b788e7103d3b742e29dce8209c0748551
Yxwww <yuxi.wangcd@gmail.com>
2019-05-25 12:16:22 -0600

    Add createShader, program, and position buffer

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,77 @@
-let gl: Maybe<WebGLRenderingContext>;
+const fragShaderSrc = `
+  // fragment shaders don't have a default precision so we need
+  // to pick one. mediump is a good default
+  precision mediump float;
+ 
+  void main() {
+    // gl_FragColor is a special variable a fragment shader
+    // is responsible for setting
+    gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
+  }
+ 
+
+`
+
+const vertextShaderSrc = `
+  // an attribute will receive data from a buffer
+  attribute vec4 a_position;
+ 
+  // all shaders have a main function
+  void main() {
+ 
+    // gl_Position is a special variable a vertex shader
+    // is responsible for setting
+    gl_Position = a_position;
+  }
+`
+function createProgram(gl: WebGLRenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader) {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+ 
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
 
 export function getGLRenderingContext(canvas: HTMLCanvasElement): WebGLRenderingContext {
     return canvas.getContext('webgl');
 }
 
-export function hereWeGo(canvas: HTMLCanvasElement) {
-    gl = getGLRenderingContext(canvas);
-    console.log(gl);
+
+function createShader(gl:WebGLRenderingContext, type: number, source:string) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+ 
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
 }
+
+export function hereWeGo(canvas: HTMLCanvasElement) {
+    const gl = getGLRenderingContext(canvas);
+    const vertextShader = createShader(gl, gl.VERTEX_SHADER, vertextShaderSrc);
+    const fragShader = createShader(gl, gl.FRAGMENT_SHADER, fragShaderSrc);
+    const program = createProgram(gl, vertextShader, fragShader);
+
+    const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    var positions = [
+        0, 0,
+        0, 0.5,
+        0.7, 0,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+}
+
+

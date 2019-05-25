2a71f17f2a85782e54654ae27b93cf5e90a99724
Yxwww <yuxi.wangcd@gmail.com>
2019-05-25 15:07:08 -0600

    Draw an rectangle

--- src/app.tsx
+++ src/app.tsx
@@ -14,14 +14,27 @@ const fragShaderSrc = `
 
 const vertextShaderSrc = `
   // an attribute will receive data from a buffer
-  attribute vec4 a_position;
+  attribute vec2 a_position;
+
+  uniform vec2 u_resolution;
  
   // all shaders have a main function
   void main() {
  
     // gl_Position is a special variable a vertex shader
     // is responsible for setting
-    gl_Position = a_position;
+    // gl_Position = a_position;
+
+    // convert the position from pixels to 0.0 to 1.0
+    vec2 zeroToOne = a_position / u_resolution;
+ 
+    // convert from 0->1 to 0->2
+    vec2 zeroToTwo = zeroToOne * 2.0;
+ 
+    // convert from 0->2 to -1->+1 (clipspace)
+    vec2 clipSpace = zeroToTwo - 1.0;
+ 
+    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
   }
 `
 function createProgram(gl: WebGLRenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader) {
@@ -56,32 +69,80 @@ function createShader(gl:WebGLRenderingContext, type: number, source:string) {
   gl.deleteShader(shader);
 }
 
+function resize(canvas: HTMLCanvasElement) {
+  // Lookup the size the browser is displaying the canvas.
+  var displayWidth  = canvas.clientWidth;
+  var displayHeight = canvas.clientHeight;
+ 
+  // Check if the canvas is not the same size.
+  if (canvas.width  != displayWidth ||
+      canvas.height != displayHeight) {
+ 
+    // Make the canvas the same size
+    canvas.width  = displayWidth;
+    canvas.height = displayHeight;
+  }
+}
+
 export function hereWeGo(canvas: HTMLCanvasElement) {
     const gl = getGLRenderingContext(canvas);
     const vertextShader = createShader(gl, gl.VERTEX_SHADER, vertextShaderSrc);
     const fragShader = createShader(gl, gl.FRAGMENT_SHADER, fragShaderSrc);
     const program = createProgram(gl, vertextShader, fragShader);
 
+    // const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    // const positionBuffer = gl.createBuffer();
+    // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    //
+    // var positions = [
+    //     0, 0,
+    //     0, 0.5,
+    //     0.7, 0,
+    // ];
+    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    //
+    // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    // gl.clearColor(0, 0, 0, 0);
+    // gl.clear(gl.COLOR_BUFFER_BIT);
+    // gl.useProgram(program);
+    // gl.enableVertexAttribArray(positionAttributeLocation);
+    // // Bind the position buffer.
+    // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    //
+    // // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    // var size = 2;          // 2 components per iteration
+    // var type = gl.FLOAT;   // the data is 32bit floats
+    // var normalize = false; // don't normalize the data
+    // var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    // var offset = 0;        // start at the beginning of the buffer
+    // gl.vertexAttribPointer(
+    //     positionAttributeLocation, size, type, normalize, stride, offset)
+    //
+    // var primitiveType = gl.TRIANGLES;
+    // var offset = 0;
+    // var count = 3;
+    // gl.drawArrays(primitiveType, offset, count);
+
+
     const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
     const positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
+    var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
     var positions = [
-        0, 0,
-        0, 0.5,
-        0.7, 0,
+        10, 20,
+        80, 20,
+        10, 30,
+        10, 30,
+        80, 20,
+        80, 30,
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.useProgram(program);
     gl.enableVertexAttribArray(positionAttributeLocation);
-    // Bind the position buffer.
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+// Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
     var size = 2;          // 2 components per iteration
     var type = gl.FLOAT;   // the data is 32bit floats
     var normalize = false; // don't normalize the data
@@ -89,9 +150,12 @@ export function hereWeGo(canvas: HTMLCanvasElement) {
     var offset = 0;        // start at the beginning of the buffer
     gl.vertexAttribPointer(
         positionAttributeLocation, size, type, normalize, stride, offset)
+
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+    // gl.enableVertexAttribArray(positionAttributeLocation);
     var primitiveType = gl.TRIANGLES;
     var offset = 0;
-    var count = 3;
+    var count = 6;
     gl.drawArrays(primitiveType, offset, count);
 }
 

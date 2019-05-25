f25cc2f6f10bb1de196c4a40f0ae477ef24ee0ad
Yxwww <yuxi.wangcd@gmail.com>
2019-05-25 10:42:56 -0600

    Setup webpack dev env with TSC

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const haha = 'hello';
+
+// if (module.hot) {
+//     module.hot.accept('./print.js', function() {
+//         console.log('Accepting the updated printMe module!');
+//         printMe();
+//     })
+// }
+export default haha;
